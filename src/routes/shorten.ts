bc3688c6d5a30640e93198a74f387b613e212260
hichemghoualmi2019 <hichemghoualmi2019@gmail.com>
2024-10-01 15:58:21 -0400

    test123

--- src/routes/shorten.ts
+++ src/routes/shorten.ts
@@ -5,9 +5,13 @@ import { loadDataFromFile2 } from '../models/dataUtils';
 const router: Router = express.Router();
 const urlShortener = new UrlShortener();
 
+// shorten the url
 router.post('/', (req, res) => urlShortener.shortenURL(req, res));
+
 router.post('/one', (req, res) => urlShortener.shortenOneURL(req, res));
 router.get('/data', loadDataFromFile2);
+
+// redirect to url suing the short key 123
 router.get('/:key', (req, res) => urlShortener.redirectToURL(req, res));
 
 export default router;
